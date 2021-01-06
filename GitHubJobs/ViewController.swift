48594cea265e2bebfdc6703f6387110c99bfc233
Sullivan677 <decarli.sulli@gmail.com>
2021-01-06 21:21:59 +0100

    Initial Commit

---
+++ GitHubJobs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubJobs
+//
+//  Created by Sullivan De carli on 06/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
